ff1241a8946196996cb64e643a9a166d17fcb5a0
demontricer <38652605+demontricer@users.noreply.github.com>
2018-06-19 09:47:30 +0000

    Create frst_1.c

---
+++ c/frst_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+long int a;
+scanf("%ld",&a);
+if(a>0 && a<=100000)
+{
+printf("Positive");
+}
+else if(a==0)
+{
+ printf("Zero"); 
+}
+else
+{
+  printf("Negative");
+}
+ return 0;
+}
