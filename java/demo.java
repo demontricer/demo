c7f120336ae78536dfcd20b155feec7e4610a457
demontricer <38652605+demontricer@users.noreply.github.com>
2018-06-26 10:10:13 +0000

    Create demo.java

---
+++ java/demo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class demo
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int sum=0;
+for(int i=0;i<=n;i++)
+sum+=i;
+System.out.print(sum);
+}
+}
